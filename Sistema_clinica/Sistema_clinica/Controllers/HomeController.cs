e4fec004dc80bf0006afcc46254cbc148b6b82f1
Ana Rizzi <ana.rizzi.eu@gmail.com>
2020-05-24 15:43:35 -0300

    Inicio do projeto - ideias iniciais

---
+++ Sistema_clinica/Sistema_clinica/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Sistema_clinica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema_clinica.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Usuario usuario)
+        {
+            if (ModelState.IsValid) {
+                return RedirectToAction("TelaPrincipal", "Home");
+            }
+            return View();
+        }
+
+        public ActionResult TelaPrincipal()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sistema_clinica/Sistema_clinica/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sistema_clinica.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage ="Campo não pode ficar em branco")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Campo não pode ficar em branco")]
+        public string Senha { get; set; }
+    }
+}
